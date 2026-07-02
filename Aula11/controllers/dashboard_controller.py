1054f62cee7b6412ad6ceff70126c21d0ae408ae
JOAO_PEDRO_CARDOSO_DUARTE <22401792@DELTAF27.FLORESTA>
2026-07-02 08:23:28 -0300

    Adicionando a pasta Aula11

---
+++ Aula11/controllers/dashboard_controller.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+from models import ClienteLocadora, Veiculo, Locacao
+
+dashboard_bp = Blueprint("dashboard", __name__)
+
+@dashboard_bp.route("/")
+def index():
+    return render_template("index.html",
+        total_clientes=ClienteLocadora.query.count(),
+        total_veiculos=Veiculo.query.count(),
+        total_locacoes=Locacao.query.count(),
+    )
